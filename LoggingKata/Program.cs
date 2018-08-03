b50de7f3d307a47b895f19bdfc40ece7ff3a6a03
Jake Watson <jakejakewatsonwatson@gmail.com>
2018-08-03 12:35:12 -0500

    Initial Commit of this branch

--- LoggingKata/Program.cs
+++ LoggingKata/Program.cs
@@ -13,17 +13,45 @@ namespace LoggingKata
         static void Main(string[] args)
         {
             logger.LogInfo("Log initialized");
+            TacoParser tacoParser = new TacoParser();
+            var lines = File.ReadAllLines(csvPath);   //This line calls the File class and uses the method 'ReadAllLines' to read all lines of our .csv file
 
-            var lines = File.ReadAllLines(csvPath);
+            double distance = 0;    //All of these variables are needed later in the program :)
+            double maxDistance = 0;
+            string store1 = "";
+            string store2 = "";
 
-            logger.LogInfo($"Lines: {lines[0]}");
+            var locations = lines.Select(tacoParser.Parse); //This uses each 'var lines' from line 17, and sends them through our TacoParser class
 
-            var parser = new TacoParser();
+            foreach (var line in locations)  //This foreach loop goes through each of our parsed locations, and assigns them a GeoCoordinate as our 'Origin point'
+            {
+                GeoCoordinate Geo1 = new GeoCoordinate(); //Don't ask how I figured this part out, trial and error
 
-            var locations = lines.Select(parser.Parse);
+                Geo1.Latitude = line.Location.Latitude; //To set up a GeoCoordinate we have to assign the Lat and Lon as seperate values 
+                Geo1.Longitude = line.Location.Longitude;
+                
+                foreach (var line2 in locations) //This foreach loop goes through all of our parsed locations again, to give us a GeoCoordinate to compare to our 'Origin point' on line 26
+                {
+                    GeoCoordinate Geo2 = new GeoCoordinate();
 
-            // TODO:  Find the two Taco Bells in Alabama that are the furthest from one another.
-            // HINT:  You'll need two nested forloops
+                    Geo2.Latitude = line2.Location.Latitude;
+                    Geo2.Longitude = line2.Location.Longitude;
+
+                    distance = Geo1.GetDistanceTo(Geo2); //This method was in our ReadMe file,and is a part of the GeoCoordinate class
+
+                    if(maxDistance < distance) //This compares our current Largest distance to the distance we just got above.
+                    {
+                        store1 = line.Name; //If our new distance is greater, we need to update the distance and the stores it was between
+                        store2 = line2.Name;
+
+                        maxDistance = distance;
+                    }
+                }
+            }
+
+            Console.WriteLine($"The Taco Bells furthest away from each other are:"); //Possibly a better way to say this? Not sure
+            Console.WriteLine($"{store1} and the {store2}, at {maxDistance} meters apart"); //Also, not sure that the distance is in meters, or correct. I checked online the distance betwen the two stores and it was 591.41 kilometers.
+            Console.ReadLine();
         }
     }
-}
+} // Snickerdoodle
--- LoggingKata/TacoParser.cs
+++ LoggingKata/TacoParser.cs
@@ -6,13 +6,28 @@
     public class TacoParser
     {
         readonly ILog logger = new TacoLogger();
-        
+
         public ITrackable Parse(string line)
         {
-            logger.LogInfo("Begin parsing");
+            var cells = line.Split(','); //This splits the line we just recieved from our .csv  file into an array based on where we have a comma
+            TacoBell tacoBell = new TacoBell(); //This creates a new TacoBell that we can assign a location and name to
 
-            // Do not fail if one record parsing fails, return null
-            return null; // TODO Implement
+            if (cells.Length != 3) //Checks  to make sure the line we received when split has just 3 elements, Lat, Lon, and a store name
+            {
+                return null; //I think this is called an 'Early out'
+            }
+
+            string lat = cells[0]; //Assigns the first and second element to the Lat and Lon, or first and second element in the array, respectively
+            string lon = cells[1];
+            string storeName = cells[2]; //Assigns the last element to the TacoBells store name
+            
+            tacoBell.Name = storeName;
+            double.TryParse(lat, out double doubleLat); //Parses the Lat and Lon, which are at first a string, into a double
+            double.TryParse(lon, out double doubleLon);
+
+            tacoBell.Location = new Point { Longitude = doubleLon, Latitude = doubleLat }; //This sets the value of our TacoBell location to the parsed Lat and Lon
+            
+            return tacoBell;
         }
     }
 }
