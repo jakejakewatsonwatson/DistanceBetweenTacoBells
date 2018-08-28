f6431ebb3590f60636fa6379944b9d68bc18dc89
Jake Watson <jakejakewatsonwatson@gmail.com>
2018-08-28 08:18:38 -0500

    Updated the tacoParser tests

--- LoggingKata.Test/TacoParserTests.cs
+++ LoggingKata.Test/TacoParserTests.cs
@@ -1,5 +1,6 @@
 using System;
 using Xunit;
+using LoggingKata;
 
 namespace LoggingKata.Test
 {
@@ -8,13 +9,25 @@ namespace LoggingKata.Test
         [Fact]
         public void ShouldDoSomething()
         {
+            Console.WriteLine("Did something");
             // TODO: Complete Something, if anything
         }
 
         [Theory]
-        [InlineData("Example")]
-        public void ShouldParse(string str)
+        [InlineData(33.564151, -84.413382, "Taco Bell Riverdale", "33.564151, -84.413382, Taco Bell Riverdale")]
+        public void ShouldParse(int lat, int lon, string name, string csvTest)
         {
+            TacoParser taco = new TacoParser();
+            Point point = new Point();
+            TacoBell bell = new TacoBell();
+            point.Latitude = lat;
+            point.Longitude = lon;
+            bell.Name = name;
+            bell.Location = point;
+            ITrackable test = taco.Parse(csvTest);
+            Assert.Equal(bell.Location.Latitude, test.Location.Latitude);
+            Assert.Equal(bell.Location.Longitude, test.Location.Longitude);
+            Assert.Equal(bell.Name, test.Name);
             // TODO: Complete Should Parse
         }
 
@@ -23,6 +36,9 @@ namespace LoggingKata.Test
         [InlineData("")]
         public void ShouldFailParse(string str)
         {
+            TacoParser taco = new TacoParser();
+            ITrackable answer = taco.Parse(str);
+            Assert.Equal(null, answer);
             // TODO: Complete Should Fail Parse
         }
     }
